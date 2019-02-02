2f016e6f4799c04ddf5174cdb66e9737b2d19827
samims <sam91v@gmail.com>
2019-02-02 21:17:43 +0530

    views for crudl

--- status/api/views.py
+++ status/api/views.py
@@ -1,10 +1,33 @@
 import json
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from django.shortcuts import get_object_or_404
 
 from status.models import Status
 from .serializers import StatusSerializer
+
+
+class StatusAPIDetailView(UpdateModelMixin, DestroyModelMixin, RetrieveAPIView):
+    permission_classes = []
+    authentication_classes = []
+    serializer_class = StatusSerializer
+    queryset = Status.objects.all()
+    lookup_field = 'id'
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    # it will avoid err if object not found
+    # def perform_destroy(self, instance):
+    #     if instance is not None:
+    #         return instance.delete()
+    #     return None
 
 
 def is_json(json_data):
@@ -16,96 +39,15 @@ def is_json(json_data):
     return is_valid
 
 
-class StatusAPIView(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, ListAPIView):
+class StatusAPIView(CreateModelMixin, ListAPIView):
     permission_classes = []
     authentication_classes = []
 
+    queryset = Status.objects.all()
     serializer_class = StatusSerializer
-    padded_id = None
-
-    def __init__(self):
-        super(StatusAPIView, self).__init__()
-        self.passed_id = None
-
-    def perform_destroy(self, instance):
-        if instance is not None:
-            return instance.delete()
-        return None
-
-    def get_queryset(self):
-        qs = Status.objects.all()
-        query = self.request.GET.get('q')
-        if query:
-            qs = qs.filter(content__icontains=query)
-        return qs
-
-    def get_object(self):
-        request = self.request
-        passed_id = request.GET.get('id') or self.passed_id
-        queryset = self.get_queryset()
-        obj = None
-        if passed_id is not None:
-            obj = get_object_or_404(queryset, id=passed_id)
-            self.check_object_permissions(request, obj)
-        return obj
-
-    def get(self, request, *args, **kwargs):
-        url_passed_id = request.GET.get('id')
-        json_data = {}
-        body_ = request.body
-        print(request.body)
-        if is_json(body_):
-            json_data = json.loads(request.body)
-        new_passed_id = json_data.get('id')
-        passed_id = url_passed_id or new_passed_id
-        self.passed_id = passed_id
-        if passed_id is not None:
-            return self.retrieve(request, *args, **kwargs)
-        return super(StatusAPIView, self).get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        url_passed_id = request.GET.get('id')
-        json_data = {}
-        body_ = request.body
-        if is_json(body_):
-            json_data = json.loads(request.body)
-        new_passed_id = json_data.get('id')
-        passed_id = url_passed_id or new_passed_id
-        print(passed_id)
-        self.passed_id = passed_id
         return self.create(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        url_passed_id = request.GET.get('id')
-        json_data = {}
-        body_ = request.body
-        if is_json(body_):
-            json_data = json.loads(request.body)
-        new_passed_id = json_data.get('id')
-        passed_id = url_passed_id or new_passed_id
-        print(passed_id)
-        self.passed_id = passed_id
-        return self.update(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs):
-        url_passed_id = request.GET.get('id')
-        json_data = {}
-        body_ = request.body
-        if is_json(body_):
-            json_data = json.loads(request.body)
-        new_passed_id = json_data.get('id')
-        passed_id = url_passed_id or new_passed_id
-        print(passed_id)
-        self.passed_id = passed_id
-        return self.partial_update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        url_passed_id = request.GET.get('id')
-        json_data = {}
-        body_ = request.body
-        if is_json(body_):
-            json_data = json.loads(request.body)
-        new_passed_id = json_data.get('id')
-        passed_id = url_passed_id or new_passed_id
-        self.passed_id = passed_id
-        return self.destroy(request, *args, **kwargs)
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
