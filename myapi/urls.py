dc80f7f9229fe28241a74ff315f20651fa27f1e8
samiul <samiul@digitalaptech.com>
2019-02-05 19:42:43 +0530

    working

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/auth/jwt/$', obtain_jwt_token),
     url(r'^api/updates/', include('updates.api.urls')),
     url(r'^api/books/', include('books.api.urls')),
     url(r'^api/status/', include('status.api.urls')),
--- scripts/cfe_rest_framework.py
+++ scripts/cfe_rest_framework.py
@@ -6,41 +6,49 @@ ENDPOINT = "http://127.0.0.1:8000/api/status/"
 
 image_path = os.path.join(os.getcwd(), "logo.jpg")
 
+get_endpoint = ENDPOINT + str(14)
+r = requests.get(get_endpoint)
+print(r.status_code)
 
-def do_img(method='get', data={}, is_json=True, image_path=None):
-    headers = {}
-    if is_json:
-        headers['content-type'] = 'application/json'
-        data = json.dumps(data)
-    if image_path is not None:
-        with open(image_path, 'rb') as image:
-            file_data = {
-                'image': image
-            }
-            r = requests.request(method, ENDPOINT, data=data, files=file_data, headers=headers)
-    else:
-        r = requests.request(method, ENDPOINT, data=data, headers=headers)
-    print(r.text)
-    print(r.status_code)
-    return r
+post_data = json.dumps({"contents": "Some random contents"})
+post_headers = json.dumps({'content-type': 'application/json'})
+r = requests.post(ENDPOINT, data=post_data)
+print(r.text, "status: ", r.status_code)
 
-
-do_img(method='put', data={'user': 1, 'id': 1, 'content': 'First post', }, is_json=False, image_path=image_path)
-
-
-def do(method='get', data={}, is_json=True):
-    headers = {}
-    if is_json:
-        headers['content-type'] = 'application/json'
-        data = json.dumps(data)
-    r = requests.request(method, ENDPOINT, data=data, headers=headers)
-    print(r.text)
-    print(r.status_code)
-    # print(r.json())
-    return r
-
-# do(data={'id': 7})
-
-# do(method='delete', data={'id': 7})
-# do(method='put', data={'id': 6, 'user': 1, 'content': 'Changing through script'})
-# do(method='post', data={'user': 1, 'content': 'Posted through scriptsss'})
+# def do_img(method='get', data=dict(), is_json=True, image_path=None):
+#     headers = {}
+#     if is_json:
+#         headers['content-type'] = 'application/json'
+#         data = json.dumps(data)
+#     if image_path is not None:
+#         with open(image_path, 'rb') as image:
+#             file_data = {
+#                 'image': image
+#             }
+#             r = requests.request(method, ENDPOINT, data=data, files=file_data, headers=headers)
+#     else:
+#         r = requests.request(method, ENDPOINT, data=data, headers=headers)
+#     print(r.text)
+#     print(r.status_code)
+#     return r
+#
+#
+# do_img(method='put', data={'user': 1, 'id': 1, 'content': 'First post', }, is_json=False, image_path=image_path)
+#
+#
+# def do(method='get', data={}, is_json=True):
+#     headers = {}
+#     if is_json:
+#         headers['content-type'] = 'application/json'
+#         data = json.dumps(data)
+#     r = requests.request(method, ENDPOINT, data=data, headers=headers)
+#     print(r.text)
+#     print(r.status_code)
+#     # print(r.json())
+#     return r
+#
+# # do(data={'id': 7})
+#
+# # do(method='delete', data={'id': 7})
+# # do(method='put', data={'id': 6, 'user': 1, 'content': 'Changing through script'})
+# # do(method='post', data={'user': 1, 'content': 'Posted through scriptsss'})
