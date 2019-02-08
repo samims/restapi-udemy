e9b08925b0f8beda19e2225cecc0610f69bc6f33
samims <sam91v@gmail.com>
2019-02-09 00:55:09 +0530

    user register serializer

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,15 +1,24 @@
 from django.contrib.auth import authenticate, get_user_model
 from django.db.models import Q
-from rest_framework import permissions
+from rest_framework import permissions, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_jwt.settings import api_settings
+
+from .serializers import UserRegisterSerializer
 
 User = get_user_model()
 
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 jwt_response_payload_handler = api_settings.JWT_RESPONSE_PAYLOAD_HANDLER
+
+
+class RegisterAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
 
 
 class AuthAPIView(APIView):
@@ -38,31 +47,30 @@ class AuthAPIView(APIView):
                 return Response(response, status=201)
         return Response({"detail": " Invalid request"}, status=400)
 
-
-class RegisterAPIView(APIView):
-    permission_classes = []
-
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return Response({"detail": "You are already registeded and authenticatied"}, status=400)
-
-        data = request.data
-        username = data.get("username")
-        email = data.get("email")
-        password = data.get("password")
-        passowrd2 = data.get("password2")
-
-        if password != passowrd2:
-            return Response({"detail": "password must match."})
-        qs = User.objects.filter(
-            Q(username__iexact=username) |
-            Q(email__iexact=username)
-        )
-        if qs.exists():
-            return Response({"detail": "This username or email already exists"}, status=401)
-        user = User.objects.create_user(username=username, email=email, password=password)
-        user.save()
-        # payload = jwt_payload_handler(user)
-        # token = jwt_encode_handler(payload)
-        # response = jwt_response_payload_handler(token, user, request=request)
-        return Response(data={"detail": "Thank you for registering. Please verify your email"}, status=201)
+# class RegisterAPIView(APIView):
+#     permission_classes = []
+#
+#     def post(self, request, *args, **kwargs):
+#         if request.user.is_authenticated:
+#             return Response({"detail": "You are already registeded and authenticatied"}, status=400)
+#
+#         data = request.data
+#         username = data.get("username")
+#         email = data.get("email")
+#         password = data.get("password")
+#         passowrd2 = data.get("password2")
+#
+#         if password != passowrd2:
+#             return Response({"detail": "password must match."})
+#         qs = User.objects.filter(
+#             Q(username__iexact=username) |
+#             Q(email__iexact=username)
+#         )
+#         if qs.exists():
+#             return Response({"detail": "This username or email already exists"}, status=401)
+#         user = User.objects.create_user(username=username, email=email, password=password)
+#         user.save()
+#         # payload = jwt_payload_handler(user)
+#         # token = jwt_encode_handler(payload)
+#         # response = jwt_response_payload_handler(token, user, request=request)
+#         return Response(data={"detail": "Thank you for registering. Please verify your email"}, status=201)
--- scripts/cfe_rest_framework.py
+++ scripts/cfe_rest_framework.py
@@ -1,84 +1,40 @@
-import requests
 import json
+import requests
 import os
+
 
 AUTH_ENDPOINT = "http://127.0.0.1:8000/api/auth/register/"
 REFRESH_ENDPOINT = AUTH_ENDPOINT + "refresh/"
 ENDPOINT = "http://127.0.0.1:8000/api/status/"
-# AUTH_ENDPOINT = "http://127.0.0.1:8000/api/auth/register"
-data = {'username': "sam1",'email': "sam1@example.com", 'password': "pass1234", "password2": "pass1234"}
-headers = {
-    "content-type": "application/json",
-    # "Authorization": "JWT " + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNTQ5NjI2MTA3LCJlbWFpbCI6ImFkbWluQGV4YW1wbGUuY29tIiwib3JpZ19pYXQiOjE1NDk2MjU4MDd9.Q7PwzubNN6YcFmk3fqiuxpc1xVX0eYnfdyCDxj5kN3w'}
-}
-r = requests.post(AUTH_ENDPOINT, data=json.dumps(data), headers=headers)
-# token = r.json()['token']
-print(r)
-print(r.json())
-
-# REFRESH_ENDPOINT = AUTH_ENDPOINT + 'refresh/'
-
-# r = requests.post(url=REFRESH_ENDPOINT, data=json.dumps({'token': token}), headers=headers)
-# token = r.json()
-# print(token)
-
 
 image_path = os.path.join(os.getcwd(), "logo.jpg")
 
-# get_endpoint = ENDPOINT + str(14)
-# r = requests.get(get_endpoint)
-# print(r.status_code)
+headers = {
+    "Content-Type": "application/json",
+    #"Authorization": "JWT " + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImNmZSIsImV4cCI6MTUxMzIwNjEwOSwiZW1haWwiOiIiLCJvcmlnX2lhdCI6MTUxMzIwNTgwOX0.JCIM7Es7-pJpKVv4-OrEjCFVYsIegRxELu6YATayu7k',
+}
 
-# headers = {"Authorization": "JWT " + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNTQ5NjIxMzUzLCJlbWFpbCI6ImFkbWluQGV4YW1wbGUuY29tIiwib3JpZ19pYXQiOjE1NDk2MjEwNTN9.S36KymSxMXuQYxczpjKB1E4Jl9tcEhBiILnHr42UtE8', 'user': 'admin', 'expires': '2019-02-15T10:14:13.476219Z'}
-#
-# post_data = {"content": "Updated random contents"}
-# with open(image_path, 'rb') as image:
-#     file_data = {
-#         'image': image
-#     }
-#     # posted_response = requests.post(ENDPOINT, data=post_data, files=file_data, headers=headers)
-#     posted_response = requests.put(ENDPOINT +"25/", data=post_data, files=file_data, headers=headers)
-#     print(posted_response.text)
-#     get_endpoint = ENDPOINT + str(12)
+data = {
+    'username': 'dd',
+    'email': '2ss0@teamcfe.com',
+    'password': 'learncode',
+    'password2': 'learncode'
+}
 
-# post_headers = json.dumps({'content-type': 'application/json'})
-# r = requests.post(ENDPOINT, data=post_data)
-# print(r.text, "status: ", r.status_code)
-
-# def do_img(method='get', data=dict(), is_json=True, image_path=None):
-#     headers = {}
-#     if is_json:
-#         headers['content-type'] = 'application/json'
-#         data = json.dumps(data)
-#     if image_path is not None:
-#         with open(image_path, 'rb') as image:
-#             file_data = {
-#                 'image': image
-#             }
-#             r = requests.request(method, ENDPOINT, data=data, files=file_data, headers=headers)
-#     else:
-#         r = requests.request(method, ENDPOINT, data=data, headers=headers)
-#     print(r.text)
-#     print(r.status_code)
-#     return r
+r = requests.post(AUTH_ENDPOINT, data=json.dumps(data), headers=headers)
+print(r.text)
+# import requests
+# import json
+# import os
 #
-#
-# do_img(method='put', data={'user': 1, 'id': 1, 'content': 'First post', }, is_json=False, image_path=image_path)
-#
-#
-# def do(method='get', data={}, is_json=True):
-#     headers = {}
-#     if is_json:
-#         headers['content-type'] = 'application/json'
-#         data = json.dumps(data)
-#     r = requests.request(method, ENDPOINT, data=data, headers=headers)
-#     print(r.text)
-#     print(r.status_code)
-#     # print(r.json())
-#     return r
-#
-# # do(data={'id': 7})
-#
-# # do(method='delete', data={'id': 7})
-# # do(method='put', data={'id': 6, 'user': 1, 'content': 'Changing through script'})
-# # do(method='post', data={'user': 1, 'content': 'Posted through scriptsss'})
+# AUTH_ENDPOINT = "http://127.0.0.1:8000/api/auth/register/"
+# REFRESH_ENDPOINT = AUTH_ENDPOINT + "refresh/"
+# ENDPOINT = "http://127.0.0.1:8000/api/status/"
+# # AUTH_ENDPOINT = "http://127.0.0.1:8000/api/auth/register"
+# data = {'username': "sam1",'email': "sam1@example.com", 'password': "pass1234", "password2": "pass1234"}
+# headers = {
+#     "content-type": "application/json",
+# }
+# r = requests.post(AUTH_ENDPOINT, data=json.dumps(data), headers=headers)
+# print(r)
+# print(r.json())
